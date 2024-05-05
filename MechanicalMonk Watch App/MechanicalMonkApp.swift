d3869905d1f2739725b053e1b1b265e0ff1b001c
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2024-05-05 10:21:16 +0300

    Initial Commit

---
+++ MechanicalMonk Watch App/MechanicalMonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechanicalMonkApp.swift
+//  MechanicalMonk Watch App
+//
+//  Created by a.nvlkv on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MechanicalMonk_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
